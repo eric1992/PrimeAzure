58f3230bd45ee74089d8bff2de460db0511a4a5b
Eric Lovell <eric1992@vt.edu>
2016-09-25 15:31:39 -0400

    Added Basix Interfaces for primes

---
+++ PrimeService/IPrimeGenerator.cs
@@ -0,0 +1,13 @@
+﻿using Mpir.NET;
+using System.Collections.Generic;
+
+namespace PrimeService
+{
+  interface IPrimeGenerator
+  {
+    mpz_t Get(int i);
+    mpz_t Get(mpz_t i);
+    IEnumerable<mpz_t> GetBetween(int i, int j);
+    IEnumerable<mpz_t> GetBetween(mpz_t i, mpz_t j);
+  }
+}
---
+++ PrimeService/IPrimeTester.cs
@@ -0,0 +1,10 @@
+﻿using Mpir.NET;
+
+namespace PrimeService
+{
+  interface IPrimeTester
+  {
+    bool IsPrime(int i);
+    bool IsPrime(mpz_t i);
+  }
+}
